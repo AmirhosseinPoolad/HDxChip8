35663f6d5722c624f47554068a648c2938f60750
Amirhossein Poolad <amirhossein.poolad@gmail.com>
2021-12-07 21:03:27 +0330

    All instructions done Added input system Added rendering system Needs some bugfixes to work properly

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include "stdlib.h"
 #include "string.h"
 
+#include "SDL.h"
+
 #define MEMORY_SIZE 4096
 #define PROGRAM_START 0x200
 
@@ -14,7 +16,11 @@
 #define SCREEN_WIDTH 64
 #define SCREEN_HEIGHT 32
 
+#define DEVICE_SCREEN_WIDTH 640
+#define DEVICE_SCREEN_HEIGHT 320
+
 int screenCoordinatesToIndex(unsigned char x, unsigned char y);
+void drawByteArrayToTexture(unsigned char *screen, SDL_Texture *screenTexture);
 
 int main()
 {
@@ -98,23 +104,181 @@ int main()
     for (int i = 0; i < bufferSize; i++)
         memory[PROGRAM_START + i] = romBuffer[i];
     unsigned int exit = 0;
+
+    //Setup SDL stuff
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "HDxChip8",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        DEVICE_SCREEN_WIDTH,
+        DEVICE_SCREEN_HEIGHT,
+        0);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_Texture *text = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
+    SDL_Rect rect = {0, 0, DEVICE_SCREEN_WIDTH, DEVICE_SCREEN_HEIGHT};
+    unsigned int startTicks;
+    unsigned int endTicks;
+    unsigned char keyDown;
     while (!exit) //main loop
     {
+        startTicks = SDL_GetTicks();
+        //Handle input
+        SDL_Event e;
+        while (SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT)
+                exit = 1;
+            if (e.type == SDL_KEYDOWN)
+            {
+                switch (e.key.keysym.sym)
+                {
+                case SDLK_1:
+                    key[0x1] = 1;
+                    keyDown = 0x1;
+                    break;
+                case SDLK_2:
+                    key[0x2] = 1;
+                    keyDown = 0x2;
+                    break;
+                case SDLK_3:
+                    key[0x3] = 1;
+                    keyDown = 0x3;
+                    break;
+                case SDLK_4:
+                    key[0xC] = 1;
+                    keyDown = 0xC;
+                    break;
+                case SDLK_q:
+                    key[0x4] = 1;
+                    keyDown = 0x4;
+                    break;
+                case SDLK_w:
+                    key[0x5] = 1;
+                    keyDown = 0x5;
+                    break;
+                case SDLK_e:
+                    key[0x6] = 1;
+                    keyDown = 0x6;
+                    break;
+                case SDLK_r:
+                    key[0xD] = 1;
+                    keyDown = 0xD;
+                    break;
+                case SDLK_a:
+                    key[0x7] = 1;
+                    keyDown = 0x7;
+                    break;
+                case SDLK_s:
+                    key[0x8] = 1;
+                    keyDown = 0x8;
+                    break;
+                case SDLK_d:
+                    key[0x9] = 1;
+                    keyDown = 0x9;
+                    break;
+                case SDLK_f:
+                    key[0xE] = 1;
+                    keyDown = 0xE;
+                    break;
+                case SDLK_z:
+                    key[0xA] = 1;
+                    keyDown = 0xA;
+                    break;
+                case SDLK_x:
+                    key[0x0] = 1;
+                    keyDown = 0x0;
+                    break;
+                case SDLK_c:
+                    key[0xB] = 1;
+                    keyDown = 0xB;
+                    break;
+                case SDLK_v:
+                    key[0xF] = 1;
+                    keyDown = 0xF;
+                    break;
+                }
+            }
+            if (e.type == SDL_KEYUP)
+            {
+                switch (e.key.keysym.sym)
+                {
+                case SDLK_1:
+                    key[0x1] = 0;
+                    break;
+                case SDLK_2:
+                    key[0x2] = 0;
+                    break;
+                case SDLK_3:
+                    key[0x3] = 0;
+                    break;
+                case SDLK_4:
+                    key[0xC] = 0;
+                    break;
+                case SDLK_q:
+                    key[0x4] = 0;
+                    break;
+                case SDLK_w:
+                    key[0x5] = 0;
+                    break;
+                case SDLK_e:
+                    key[0x6] = 0;
+                    break;
+                case SDLK_r:
+                    key[0xD] = 0;
+                    break;
+                case SDLK_a:
+                    key[0x7] = 0;
+                    break;
+                case SDLK_s:
+                    key[0x8] = 0;
+                    break;
+                case SDLK_d:
+                    key[0x8] = 0;
+                    break;
+                case SDLK_f:
+                    key[0xE] = 0;
+                    break;
+                case SDLK_z:
+                    key[0xA] = 0;
+                    break;
+                case SDLK_x:
+                    key[0x0] = 0;
+                    break;
+                case SDLK_c:
+                    key[0xB] = 0;
+                    break;
+                case SDLK_v:
+                    key[0xF] = 0;
+                    break;
+                }
+            }
+        }
         //each element is one byte, so we shift the first bite
         //into the most significant byte and the one after it into the least significant byte of opcode
         opcode = memory[PC] << 8 | memory[PC + 1];
         switch (opcode & 0xF000)
         {
         case 0x0000:
-            switch (opcode & 0x000F)
+            switch (opcode & 0x00FF)
             {
-            case 0x0000: //Clear the display
-                memset(screen, 0, sizeof(screen));
+            case 0x00E0: //00E0 --- Clear the display
+                //memset(screen, 0, sizeof(screen));
                 PC += 2;
                 break;
-            case 0x000E:        //return from a subroutine
-                PC = stack[SP]; //program counter = address at top of the stack
-                SP--;           //remove top of the stack
+            case 0x00EE: //00EE --- eturn from a subroutine
+                if (SP != 0)
+                {
+                    PC = stack[SP]; //program counter = address at top of the stack
+                    SP--;           //remove top of the stack
+                }
+                else
+                {
+                    PC += 2;
+                }
                 break;
             }
             break;
@@ -129,14 +293,20 @@ int main()
         case 0x3000: //3xkk --- skip next instruction if Vx == kk
             if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
                 PC += 4; //two for this execution, 2 for the next
+            else
+                PC += 2;
             break;
         case 0x4000: //4xkk --- skip next instruction if Vx != kk
             if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
                 PC += 4;
+            else
+                PC += 2;
             break;
         case 0x5000: //5xy0 --- skip next instruction if Vx == Vy
             if (V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4])
                 PC += 4;
+            else
+                PC += 2;
             break;
         case 0x6000: //6xkk --- set Vx = kk
             V[(opcode & 0x0F00) >> 8] = (opcode & 0x00FF);
@@ -149,6 +319,10 @@ int main()
         case 0x8000:
             switch (opcode & 0x000F)
             {
+            case 0x0000: //8xy0 --- Set Vx = Vy
+                V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
+                PC += 2;
+                break;
             case 0x0001: //8xy1 --- Set Vx = Vx OR Vy
                 V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] | V[(opcode & 0x00F0) >> 4];
                 PC += 2;
@@ -213,7 +387,7 @@ int main()
                 PC += 2;
                 break;
             case 0x000E: //8xyE --- Set Vx = Vx SHL 1 (carry in VF)
-                if ((V[(opcode & 0x0F00) >> 8] & 0xA000) == 0xA000)
+                if ((V[(opcode & 0x0F00) >> 8] & 0x8000) == 0x8000)
                 {
                     V[0xF] = 1;
                 }
@@ -229,6 +403,8 @@ int main()
         case 0x9000: //9xy0 --- Skip next instruction if Vx != Vy
             if (V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4])
                 PC += 4;
+            else
+                PC += 2;
             break;
         case 0xA000: //Annn --- Set I = nnn
             I = opcode & 0x0FFF;
@@ -247,8 +423,8 @@ int main()
         case 0xD000: //Dxyn --- Display n-byte (n rows) sprite starting at memory location I at (Vx, Vy), set VF = collision.
             //Drawing is done by XORing the sprite with the screen. If any pixel gets erased during this a collision has happened
             {
-                unsigned short x = (opcode & 0x0F00) >> 8;
-                unsigned short y = (opcode & 0x00F0) >> 4;
+                unsigned short x = V[(opcode & 0x0F00) >> 8];
+                unsigned short y = V[(opcode & 0x00F0) >> 4];
                 unsigned short n = (opcode & 0x000F);
                 for (int j = 0; j < n; j++)
                 {
@@ -299,8 +475,11 @@ int main()
                 PC += 2;
                 break;
             case 0x000A: //Fx0A --- Wait for a key press, store the value of the key in Vx.
-                //TODO
-                PC += 2;
+                if (keyDown != 0)
+                {
+                    V[(opcode & 0x0F00) >> 8] = keyDown;
+                    PC += 2;
+                }
                 break;
             case 0x0015: //Fx15 --- Set delay timer = Vx.
                 delay_timer = V[(opcode & 0x0F00) >> 8];
@@ -357,6 +536,18 @@ int main()
         default:
             break;
         }
+        if (delay_timer > 0)
+            delay_timer--;
+        if (sound_timer > 0)
+            sound_timer--;
+        drawByteArrayToTexture(screen, text);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, text, NULL, NULL);
+        SDL_RenderPresent(renderer);
+        endTicks = SDL_GetTicks();
+        if ((endTicks - startTicks) < 16)
+            SDL_Delay(16 - (endTicks - startTicks));
+        //SDL_Delay(150);
     }
 }
 
@@ -368,4 +559,25 @@ int screenCoordinatesToIndex(unsigned char x, unsigned char y)
     if (y >= SCREEN_HEIGHT)
         finalY = y % SCREEN_HEIGHT;
     return (finalY * SCREEN_WIDTH) + finalX;
+}
+
+void drawByteArrayToTexture(unsigned char *screen, SDL_Texture *screenTexture)
+{
+    uint32_t textureBuffer[SCREEN_WIDTH * SCREEN_HEIGHT];
+    for (int y = 0; y < SCREEN_HEIGHT; y++)
+    {
+        for (int x = 0; x < SCREEN_WIDTH; x++)
+        {
+            int index = screenCoordinatesToIndex(x, y);
+            if (screen[index] == 1)
+            {
+                textureBuffer[index] = 0xFFFFFFFF;
+            }
+            else
+            {
+                textureBuffer[index] = 0x00000000;
+            }
+        }
+    }
+    SDL_UpdateTexture(screenTexture, NULL, textureBuffer, SCREEN_WIDTH * sizeof(uint32_t));
 }
