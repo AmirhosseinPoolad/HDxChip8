1e3ce81e31c5e95dbd7749d4ab48f7ab5a76d89b
Amirhossein Poolad <amirhossein.poolad@gmail.com>
2021-12-07 18:34:35 +0330

    25/34 opcodes implemented

--- src/main.c
+++ src/main.c
@@ -11,6 +11,11 @@
 
 #define SPRITE_WIDTH 8
 
+#define SCREEN_WIDTH 64
+#define SCREEN_HEIGHT 32
+
+int screenCoordinatesToIndex(unsigned char x, unsigned char y);
+
 int main()
 {
     //System memory
@@ -34,7 +39,8 @@ int main()
     unsigned char sound_timer = 0;
     //CHIP-8 screen is 64*32 black and white i.e. only pure black or pure white.
     //coordinates start from top left
-    unsigned char screen[64 * 32];
+    unsigned char screen[SCREEN_WIDTH * SCREEN_HEIGHT];
+    memset(screen, 0, SCREEN_WIDTH * SCREEN_HEIGHT);
     //CHIP-8 uses a hexadecimal keypad that we map to the keyboard as such:
     //Keypad                   Keyboard
     //+-+-+-+-+                +-+-+-+-+
@@ -104,6 +110,7 @@ int main()
             {
             case 0x0000: //Clear the display
                 memset(screen, 0, sizeof(screen));
+                PC += 2;
                 break;
             case 0x000E:        //return from a subroutine
                 PC = stack[SP]; //program counter = address at top of the stack
@@ -113,21 +120,207 @@ int main()
             break;
         case 0x1000: //1nnn --- jump PC to nnn
             PC = opcode & 0x0FFF;
+            break;
         case 0x2000: //2nnn --- call subroutine at nnn
             stack[SP] = PC;
             PC = opcode & 0x0FFF;
             SP++;
             break;
         case 0x3000: //3xkk --- skip next instruction if Vx == kk
-            if (V[(opcode & 0x0F00) >> 2] == (opcode & 0x00FF))
-                PC += 2;
+            if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
+                PC += 4; //two for this execution, 2 for the next
             break;
         case 0x4000: //4xkk --- skip next instruction if Vx != kk
-            if (V[(opcode & 0x0F00) >> 2] != (opcode & 0x00FF))
+            if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
+                PC += 4;
+            break;
+        case 0x5000: //5xy0 --- skip next instruction if Vx == Vy
+            if (V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4])
+                PC += 4;
+            break;
+        case 0x6000: //6xkk --- set Vx = kk
+            V[(opcode & 0x0F00) >> 8] = (opcode & 0x00FF);
+            PC += 2;
+            break;
+        case 0x7000: //7xkk --- set Vx = Vx + kk;
+            V[(opcode & 0x0F00) >> 8] += (opcode & 0x00FF);
+            PC += 2;
+            break;
+        case 0x8000:
+            switch (opcode & 0x000F)
+            {
+            case 0x0001: //8xy1 --- Set Vx = Vx OR Vy
+                V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] | V[(opcode & 0x00F0) >> 4];
                 PC += 2;
+                break;
+            case 0x0002: //8xy2 --- Set Vx = Vx AND Vy
+                V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] & V[(opcode & 0x00F0) >> 4];
+                PC += 2;
+                break;
+            case 0x0003: //8xy3 --- Set Vx = Vx XOR Vy
+                V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] ^ V[(opcode & 0x00F0) >> 4];
+                PC += 2;
+                break;
+            case 0x0004: //8xy4 --- Set Vx = Vx + Vy, set VF = carry
+                if (V[(opcode & 0x00F0) >> 4] > (0xFF - V[(opcode & 0x0F00) >> 8]))
+                //if Vy is bigger than 255 - Vx
+                {
+                    V[0xF] = 1; //carry
+                }
+                else
+                {
+                    V[0xF] = 0; //no carry
+                }
+                V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F0) >> 4];
+                PC += 2;
+                break;
+            case 0x0005: //8xy5 --- Set Vx = Vx - Vy, set VF = NOT borrow
+                if (V[(opcode & 0x00F0) >> 4] > V[(opcode & 0x0F00) >> 8])
+                //if Vy is bigger than Vx
+                {
+                    V[0xF] = 0; //borrow (no carry)
+                }
+                else
+                {
+                    V[0xF] = 1; //no borrow (carry)
+                }
+                V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
+                PC += 2;
+                break;
+            case 0x0006: //8xy6 --- Set Vx = Vx SHR 1 (carry in VF)
+                if (V[(opcode & 0x0F00) >> 8] & 0x0001)
+                {
+                    V[0xF] = 1;
+                }
+                else
+                {
+                    V[0xF] = 0;
+                }
+                V[(opcode & 0x0F00) >> 8] >> 1;
+                PC += 2;
+                break;
+            case 0x0007: //8xy7 --- Set Vx = Vy - Vx, set VF = NOT borrow
+                if (V[(opcode & 0x00F0) >> 4] < V[(opcode & 0x0F00) >> 8])
+                //if Vx is bigger than Vy
+                {
+                    V[0xF] = 0; //borrow (no carry)
+                }
+                else
+                {
+                    V[0xF] = 1; //no borrow (carry)
+                }
+                V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4] - V[(opcode & 0x0F00) >> 8];
+                PC += 2;
+                break;
+            case 0x000E: //8xyE --- Set Vx = Vx SHL 1 (carry in VF)
+                if ((V[(opcode & 0x0F00) >> 8] & 0xA000) == 0xA000)
+                {
+                    V[0xF] = 1;
+                }
+                else
+                {
+                    V[0xF] = 0;
+                }
+                V[(opcode & 0x0F00) >> 8] << 1;
+                PC += 2;
+                break;
+            }
+            break;
+        case 0x9000: //9xy0 --- Skip next instruction if Vx != Vy
+            if (V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4])
+                PC += 4;
+            break;
+        case 0xA000: //Annn --- Set I = nnn
+            I = opcode & 0x0FFF;
+            PC += 2;
+            break;
+        case 0xB000: //Bnnn --- Jump to location nnn + V0
+            PC = (opcode & 0x0FFF) + V[0x0];
+            break;
+        case 0xC000: //Cxkk --- Set Vx = random byte AND kk
+            unsigned char random = rand() % 256;
+            V[(opcode & 0x0F00) >> 8] = random & (opcode & 0x00FF);
+            PC += 2;
+        case 0xD000: //Dxyn --- Display n-byte (n rows) sprite starting at memory location I at (Vx, Vy), set VF = collision.
+            //Drawing is done by XORing the sprite with the screen. If any pixel gets erased during this a collision has happened
+            unsigned short x = (opcode & 0x0F00) >> 8;
+            unsigned short y = (opcode & 0x00F0) >> 4;
+            unsigned short n = (opcode & 0x000F);
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    //get pixel (i,j) of sprite
+                    unsigned char pixelValue = (memory[I + j] & (0x80 >> i)) >> (7 - i);
+                    //if pixel and screen are both 1
+                    if ((screen[screenCoordinatesToIndex(x + i, y + j)] == 1) && (pixelValue == 1))
+                        V[0xF] = 1; //collision
+                    //draw the pixel
+                    screen[screenCoordinatesToIndex(x + i, y + j)] ^= pixelValue;
+                }
+            }
+            PC += 2;
+            break;
+        case 0xE000:
+            switch (opcode & 0x00FF)
+            {
+            case 0x009E: //Ex9E --- Skip next instruction if key with the value of Vx is pressed.
+                if (key[V[(opcode & 0x0F00) >> 8]])
+                {
+                    PC += 4;
+                }
+                else
+                {
+                    PC += 2;
+                }
+                break;
+            case 0x00A1: //ExA1 --- Skip next instruction if key with the value of Vx is not pressed.
+                if (!key[V[(opcode & 0x0F00) >> 8]])
+                {
+                    PC += 4;
+                }
+                else
+                {
+                    PC += 2;
+                }
+                break;
+            }
+            break;
+        case 0xF000:
+            switch (opcode & 0x00FF)
+            {
+            case 0x0007: //Fx07 --- Set Vx = delay timer value.
+                break;
+            case 0x000A: //Fx0A --- Wait for a key press, store the value of the key in Vx.
+                break;
+            case 0x0015: //Fx15 --- Set delay timer = Vx.
+                break;
+            case 0x0018: //Fx18 --- Set sound timer = Vx.
+                break;
+            case 0x001E: //Fx1E --- Set I = I + Vx.
+                break;
+            case 0x0029: //Fx29 --- Set I = location of sprite for digit Vx.
+                break;
+            case 0x0033: //Fx33 --- Store BCD representation of Vx in memory locations I, I+1, and I+2.
+                break;
+            case 0x0055: //Fx55 --- Store registers V0 through Vx in memory starting at location I.
+                break;
+            case 0x0065: //Fx65 --- Read registers V0 through Vx from memory starting at location I.
+                break;
+            }
             break;
         default:
             break;
         }
     }
+}
+
+int screenCoordinatesToIndex(unsigned char x, unsigned char y)
+{
+    int finalX = x, finalY = y;
+    if (x >= SCREEN_WIDTH)
+        finalX = x % SCREEN_WIDTH;
+    if (y >= SCREEN_HEIGHT)
+        finalY = y % SCREEN_HEIGHT;
+    return (finalY * SCREEN_WIDTH) + finalX;
 }
